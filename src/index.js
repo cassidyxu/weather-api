e82d0cc0ce7d6f1a1c21328cb3b1aac6e3ad5bea
cassidyxu <68167806+cassidyxu@users.noreply.github.com>
2022-03-19 23:15:43 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,36 @@
+let weather = {
+  apiKey: "6760a6fcffdc3f186024d277bddd4caa",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather found.");
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { description } = data.weather[0];
+    const { temp } = data.main;
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  }
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
